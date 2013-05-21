47b28ff22f5a417eb06782ee3db0d800bac7881c
maddinat0r <maddin4t0r@gmail.com>
2013-05-21 20:28:39 +0200

    v0.3.1

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData) {
 	WSAStartup(MAKEWORD(2,0), &wsa_unused);
 #endif
 
-	logprintf("TSConnector v0.3 loaded.");
+	logprintf("TSConnector v0.3.1 loaded.");
 	return 1;
 }
 
