e5052f583130c09becf352435a586689cee91a2a
maddinat0r <maddin4t0r@gmail.com>
2014-09-03 15:28:05 +0200

    bugfix: callbacks with empty format specifier didn't work

--- src/CCallback.cpp
+++ src/CCallback.cpp
@@ -50,38 +50,42 @@ void CCallbackHandler::Process()
 CCallback *CCallbackHandler::Create(string name, string format, 
 	AMX* amx, cell* params, const cell param_offset)
 {
-	if (amx == nullptr || params == nullptr || name.empty() || format.empty())
-		return nullptr;
-
-	if (static_cast<cell>(params[0]/sizeof(cell)) < param_offset)
+	if (name.empty())
 		return nullptr;
 
 
-	cell param_idx = 0;
-	cell *address_ptr = nullptr;
 	decltype(CCallback::m_Params) param_list;
-
-	for (auto &c : format)
+	if (format.empty() == false)
 	{
-		switch (c)
-		{
-		case 'd':
-		case 'i':
-		case 'f':
-		case 'b':
-			amx_GetAddr(amx, params[param_offset + param_idx], &address_ptr);
-			param_list.push(*address_ptr);
-			break;
-		case 's':
-			param_list.push(amx_GetCppString(amx, params[param_offset + param_idx]));
-			break;
-		default:
-			logprintf(">> plugin.TSConnector: Error while creating callback structure: \
-					  (#1) Unrecognized format specifier '%c'", c);
+		if (amx == nullptr || params == nullptr 
+			|| static_cast<cell>(params[0] / sizeof(cell)) < param_offset)
 			return nullptr;
-		}
-		param_idx++;
-	}
 
+
+		cell param_idx = 0;
+		cell *address_ptr = nullptr;
+
+		for (auto &c : format)
+		{
+			switch (c)
+			{
+			case 'd':
+			case 'i':
+			case 'f':
+			case 'b':
+				amx_GetAddr(amx, params[param_offset + param_idx], &address_ptr);
+				param_list.push(*address_ptr);
+				break;
+			case 's':
+				param_list.push(amx_GetCppString(amx, params[param_offset + param_idx]));
+				break;
+			default:
+				logprintf(">> plugin.TSConnector: Error while creating callback structure: \
+						  (#1) Unrecognized format specifier '%c'", c);
+				return nullptr;
+			}
+			param_idx++;
+		}
+	}
 	return new CCallback(name, std::move(param_list));
 }
